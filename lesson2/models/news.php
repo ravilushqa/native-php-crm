8caaf2e6f30677446388192e43fff6224ad15759
ravil <m1Lkkkkkke@yandex.ru>
2016-04-07 00:36:19 +0300

    Абстрактная модель и LSB

--- lesson2/models/news.php
+++ lesson2/models/news.php
@@ -1,23 +1,13 @@
 <?php
 
-require_once __DIR__ . '/../classes/DB.php';
 
-class News {
+class News extends AbstractModel
+{
     public $id;
     public $title;
     public $text;
 
-    public static function getAll()
-    {
-        $db = new DB;
-        return $db->queryAll('SELECT * FROM news', 'News');
-
-    }
-
-    public static function getOne($id)
-    {
-        $db = new DB();
-        return $db->queryOne('SELECT * FROM news WHERE id=' . $id, 'News');
-
-    }
+    protected static $table = 'news';
+    protected static $class = 'News';
 }
+
